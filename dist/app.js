b29a3f07bd8f04a25d5430696f3b2ac0cdff44df
javierpb275 <javierpb275@gmail.com>
2021-12-26 22:55:05 +0100

    setup project

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+//initializations
+const app = (0, express_1.default)();
+//settings
+app.set("port", process.env.PORT || 3000);
+//middlewares
+app.use((0, morgan_1.default)("dev"));
+app.use((0, cors_1.default)());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+//routes
+app.get("/", (req, res) => {
+    res.send(`API at http://localhost:${app.get("port")}`);
+});
+exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+//initializations
+const app: Application = express();
+
+//settings
+app.set("port", process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send(`API at http://localhost:${app.get("port")}`);
+});
+
+export default app;
