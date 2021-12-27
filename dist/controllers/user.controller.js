040ad9e6ca6fc5b2a8c65af71baa8d2819bad715
javierpb275 <javierpb275@gmail.com>
2021-12-27 19:03:20 +0100

    signUp function for /signup route of user routes

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,9 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signIn = exports.signUp = void 0;
-const signUp = (req, res) => {
-    res.send("signup");
-};
+const user_1 = __importDefault(require("../models/user"));
+const signUp = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.body.email || !req.body.password) {
+        return res
+            .status(400)
+            .json({ msg: "Please. Send your email and password" });
+    }
+    const user = yield user_1.default.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).json({ msg: "Wrong email. Try again" });
+    }
+    const newUser = new user_1.default(req.body);
+    yield newUser.save();
+    return res.status(201).json(newUser);
+});
 exports.signUp = signUp;
 const signIn = (req, res) => {
     res.send("signin");
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
+import User, { IUser } from "../models/user";
 
-export const signUp = (req: Request, res: Response) => {
-  res.send("signup");
+export const signUp = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .json({ msg: "Please. Send your email and password" });
+  }
+  const user: IUser | null = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({ msg: "Wrong email. Try again" });
+  }
+  const newUser: IUser = new User(req.body);
+  await newUser.save();
+  return res.status(201).json(newUser);
 };
 
 export const signIn = (req: Request, res: Response) => {
